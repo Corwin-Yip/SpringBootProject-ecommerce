213794e4143de906705898676beac6864d5f115e
corwin yip <corwin.yip@fdmgroup.com>
2024-03-31 13:07:08 +0800

    create index.html home page

--- src/main/java/com/fdmgroup/contoller/UserController.java
+++ src/main/java/com/fdmgroup/contoller/UserController.java
@@ -1,5 +1,23 @@
 package com.fdmgroup.contoller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.fdmgroup.repository.UserRepository;
+import com.fdmgroup.service.UserService;
+
+@Controller
 public class UserController {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private UserService userService;
+	
+	
+	
+	
+	
 
 }
