b5cee2235d5f56e6f620df9846fcc7b13fc4caa2
corwin yip <corwin.yip@fdmgroup.com>
2024-04-02 08:28:31 +0800

    update logger

--- src/main/java/com/fdmgroup/SpringEcommerceApplication.java
+++ src/main/java/com/fdmgroup/SpringEcommerceApplication.java
@@ -11,7 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringEcommerceApplication {
 	private static final Logger LOGGER = LogManager.getLogger(SpringEcommerceApplication.class);
 	public static void main(String[] args) {
-		
+		LOGGER.info("Application Start");
 		SpringApplication.run(SpringEcommerceApplication.class, args);
 		
 		LOGGER.info("Application Start");
